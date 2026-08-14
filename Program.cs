8d019bc7dd759f7c3e539fc45bbc06feba6b176f
Timon Studer <T.Studer.inf25@stud.bbbaden.ch>
2026-08-14 10:44:41 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Roguelike
+{
+    class Program()
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
